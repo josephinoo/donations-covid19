ee9eb2d5e76da1bb5579c7ab425a34c8b2dd1cc6
Joseph Avila Alvarez <josdavil@espol.edu.ec>
2021-01-22 16:45:51 -0500

    add:routes

--- server/src/app.py
+++ server/src/app.py
@@ -1,5 +1,6 @@
-from flask import Flask,request 
+from flask import Flask,request,jsonify 
 from flask_pymongo import PyMongo
+
 import json 
 app=Flask(__name__)
 app.config["MONGO_URI"]='mongodb://localhost/donations'
@@ -10,14 +11,30 @@ def create_user():
     password=request.json['password']
     email=request.json['email']
     if username and email and password:
-        mongo.db.users.insert({
+        id=mongo.db.users.insert({
             'username':username,
             'password':password,
             'email':email
         })
+        responde={
+            'id':str(id),
+            'username':username,
+            'password':password,
+            'email':email
+        }
+        return response
     else:
-        return {'message error'}
-    return {'message':'recived '}
+        return not_found()
+    return  {'message':'recived'}
+    
+@app.errorhandler(404)
+def not_found(error=None):
+    response=jsonify({
+        'message':'Resource Not Found'+request.url,
+        'status':404
+    })
+    response.status_code=404
+    return response
 
 if __name__=="__main__":
     app.run(debug=True)
